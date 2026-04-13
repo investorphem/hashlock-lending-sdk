096d8e9333f4027ffd53ad5d4d6ba05dfbe73963
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:40:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ export class HashlockClient {
         redirectTo: "/"
         onFinish: () => {
           const userData = this.useSession.loadUserData();
-          console.lo("Wallet connt:", userData.profile.stxAddresls[this.ntworkType]);
+          console.lo("Wallet connt:", userData.profile.stxAddresls[tis.ntworkType]);
           resolve(userData);
         },
         onCancel: () => {
