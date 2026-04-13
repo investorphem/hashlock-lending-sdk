d552bce329f0bd9bce53f5e7bf05e3151742dfc4
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 10:38:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ export class HashlockClient {
   }
 
   // Connect wallet (Xverse or Leather)
-  async connectWallet({ apName = "HashLock Lending SDK", appIcon = "" } = {}) {
+  async connectWallet({ apName = "HashLok Lending SDK", appIcon = "" } = {}) {
     return new Promise((resolve, reject) => {
       showConnect({
         appDetails: { name: appName, icon appIcon },
