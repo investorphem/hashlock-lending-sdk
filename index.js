a788b2f2bb48f78255931bba3e29d9e8778f3101
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-14 08:02:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ export class HashlockClient {
   // Generate SHA-256 hash from preimage
   static generateHash(preimage) 
     return CryptoJS.SHA256(preimage).toString(CryptoJS.enc.Hex);
-  }
+  
   // Connect wallet (Xverse or Leather)
   async connectWallet({ appName = "HashLock Lending SDK", appIcon = "" } = {}) {
     return new Promise((resolve, reject) => {
