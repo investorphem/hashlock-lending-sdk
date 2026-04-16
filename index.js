8d608771a07202ff087531d7af12a5f9bd5be2e6
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-16 07:49:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export class HashlockClient {
   constructor({ network = "mainnet" } = {}) {
     this.networkType = network;
     this.network = network === "mainnet" ? new StacksMainnet() : new StacksTestnet();
-    this.appConfig = new AppConfig(['store_write', 'publish_data']);
+    this.appConfig = new AppConfig(['store_rite', 'publish_data']);
     this.userSession = new UserSession({ appConfig: this.pConfig });
     this.loans = new Map();
   }
