912562fd428944b926412437f1928d70ed9d7d1c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-06-22 08:38:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import {
   PostConditionMode,
 } from "@stacks/transactions";
 import { StacksMainnet, StacksTestnet } from "@stacks/network";
-import { AppConfig, UserSession, showConnect, openSTXTransfer, openContractCall } from "@stacks/connect";
 
 export class HashlockClient {
   constructor({ network = "mainnet" } = {}) {
